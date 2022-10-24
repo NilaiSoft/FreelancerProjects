12c5565c67f0e6dd01a1c5c23c562509bf2ed207
Nilai <Visualstadio@gmail.com>
2022-10-24 10:15:53 +0330

    Add Unit Test Project

---
+++ FreelancerProjects.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FreelancerProjects.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
