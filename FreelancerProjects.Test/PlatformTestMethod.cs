cd8e83058154028146d2edc2fdacd5de0a710cf4
Nilai <Visualstadio@gmail.com>
2022-10-24 10:57:52 +0330

    BaseController Test

--- FreelancerProjects.Test/PlatformTestMethod.cs
+++ FreelancerProjects.Test/PlatformTestMethod.cs
@@ -3,6 +3,7 @@ using FreelancerProjects.Models;
 using FreelancerProjects.Models.ViewModels;
 using FreelancerProjects.Services;
 using FreelancerProjects.Web.Customer.BaseController;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FreelancerProjects.Test
@@ -10,6 +11,8 @@ namespace FreelancerProjects.Test
     [TestClass]
     public class PlatformTestMethod : CustomerBaseController<PlatformDevelop, PlatformDevelopModel>
     {
+        private readonly IRepository<PlatformDevelop, PlatformDevelopModel> _platformDevelopRepository;
+        private readonly IMapper _mapper;
         public PlatformTestMethod(IRepository<PlatformDevelop, PlatformDevelopModel> repository,
             IMapper mapper) : base(repository, mapper)
         {
@@ -17,9 +20,15 @@ namespace FreelancerProjects.Test
         }
 
         [TestMethod]
-        public void CreatePlatform()
+        public async void CreatePlatform()
         {
+            var controller =
+                new CustomerBaseController<PlatformDevelop, PlatformDevelopModel>
+                (_platformDevelopRepository, _mapper);
 
+            var result = await controller.Details(1) as ViewResult;
+
+            Assert.AreEqual("Details", result.ViewName);
         }
     }
 }
