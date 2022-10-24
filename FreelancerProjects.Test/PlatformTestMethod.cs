d833c59b01f8a996eaadb0dbbac11326e97b127a
Nilai <Visualstadio@gmail.com>
2022-10-24 10:30:12 +0330

    add refrences

---
+++ FreelancerProjects.Test/PlatformTestMethod.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FreelancerProjects.Models;
+using FreelancerProjects.Models.ViewModels;
+using FreelancerProjects.Services;
+using FreelancerProjects.Web.Customer.BaseController;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FreelancerProjects.Test
+{
+    [TestClass]
+    public class PlatformTestMethod : CustomerBaseController<PlatformDevelop, PlatformDevelopModel>
+    {
+        public PlatformTestMethod(IRepository<PlatformDevelop, PlatformDevelopModel> repository,
+            IMapper mapper) : base(repository, mapper)
+        {
+
+        }
+
+        [TestMethod]
+        public void CreatePlatform()
+        {
+
+        }
+    }
+}
